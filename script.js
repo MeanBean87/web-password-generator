282308f3e4d4c594ba947d7222e4df6e80155591
Michael Mattingly <mikeamatt1@hotmail.com>
2023-06-27 20:39:42 -0400

    Linked stylesheet and created filter for non-compatable data types or numbers not betweek 8-128

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const uppercaseString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const numberString = "0123456789";
 const specialString = "!@#$%^&*()_+~`|}{[]:;?><,./-=";
 
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -15,6 +14,22 @@ function writePassword() {
   passwordText.value = password;
 
 }
+
+
+const generatePassword = () => {
+  let passwordLength = Number(prompt("How many characters would you like your password to be? Must be between 8 and 128 characters"));
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Password length must be a number between 8 and 128 characters.\nex: 8 - 128");
+    generateBtn.removeEventListener("click", writePassword);
+    return null;
+  }
+
+
+
+ }
+
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
